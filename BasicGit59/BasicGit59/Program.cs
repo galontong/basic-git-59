d602f96529585daea575657302713d109572f16b
Herul Syahwandi S <34206362+galontong@users.noreply.github.com>
2021-11-12 09:57:41 +0700

    Update Program.cs

--- BasicGit59/BasicGit59/Program.cs
+++ BasicGit59/BasicGit59/Program.cs
@@ -7,6 +7,12 @@ namespace BasicGit59
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
